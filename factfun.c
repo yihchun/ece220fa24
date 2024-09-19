65f0e0944cb65f2a6c1e9f2ce45e4aff9ebb949a
Yih-Chun Hu <git@matrii.com>
2024-09-19 13:27:14 -0500

    L7

--- factfun.c
+++ factfun.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int Fact(unsigned int n);
-
+unsigned int multiply(unsigned int b, unsigned int a) {
+  unsigned int i, accum;
+  accum = 0;
+  for (i=1; i; i <<= 1) {
+    if (a & i)
+      accum += b;
+    b <<= 1;
+  }
+  return accum;
+}
+  
 int main() {
   unsigned int x;
   scanf("%u", &x);
@@ -11,7 +21,7 @@ int main() {
 int Fact(unsigned int n) {
   int total = 1;
   while (n > 0) {
-    total *= n;
+    total = multiply(n, total);
     n--;
   }
   return total;
--- runtimestack.asm
+++ runtimestack.asm
@@ -3,20 +3,114 @@
 
 LD R6, STACK_BOTTOM
 
-; push parameters onto the stack (caller setup)
 LD R0, PARAM1
 ADD R6, R6, #-1
 STR R0, R6, #0
-LD R0, PARAM2
-ADD R6, R6, #-1
-STR R0, R6, #0
+JSR FACT
+HALT
 
-JSR DO_SUM
+PARAM1 .FILL #5
+PARAM2 .FILL #3
 
+FACT
+	ADD R6, R6, #-4 ; -(3+LV)
+	STR R5, R6, #1  ; (LV)
+	ADD R5, R6, #0  ; LV-1
+	STR R7, R5, #2
 
+	; total = 1
+	AND R0, R0, #0
+	ADD R0, R0, #1
+	STR R0, R5, #0
 
-PARAM1 .FILL #3
-PARAM2 .FILL #5
+	; while n > 0
+FACT_LOOP_TOP
+	LDR R0, R5, #4
+	BRz FACT_LOOP_EXIT
+
+	; total = multiply(n, total)
+	;;; caller setup
+	ADD R6, R6, #-2 ; -(params)
+	STR R0, R6, #0
+	LDR R0, R5, #0
+	STR R0, R6, #1
+	JSR MULTIPLY
+	;;; caller teardown
+	LDR R0, R6, #0
+	ADD R6, R6, #3	; params+1
+
+	STR R0, R5, #0 ;  total = multiply(n, total)
+
+	; n--
+	LDR R0, R5, #4
+	ADD R0, R0, #-1
+	STR R0, R5, #4
+	
+	BRnzp FACT_LOOP_TOP
+	
+FACT_LOOP_EXIT
+	; return total
+	LDR R0, R5, #0
+	STR R0, R5, #3
+
+	LDR R7, R5, #2
+	LDR R5, R5, #1
+	ADD R6, R6, #3 ; LV+2
+	RET
+
+MULTIPLY
+	ADD R6, R6, #-5 ; -(3+LV)
+	STR R5, R6, #2  ; (LV)
+	ADD R5, R6, #1  ; LV-1
+	STR R7, R5, #2
+	
+	; accum = 0
+	AND R0, R0, #0
+	STR R0, R5, #-1
+
+	; i = 1
+	AND R1, R1, #0
+	ADD R1, R1, #1
+	STR R1, R5, #0
+
+MULTIPLY_LOOP_TOP
+	; check if i is nonzero
+	LDR R1, R5, #0
+	BRz MULTIPLY_LOOP_EXIT
+
+	; if (a & i)
+	LDR R0, R5, #5
+	AND R0, R1, R0
+	BRz DONT_ADD
+	
+	; accum += b
+	LDR R0, R5, #-1
+	LDR R1, R5, #4
+	ADD R0, R0, R1
+	STR R0, R5, #-1
+
+DONT_ADD
+	; b <<= 1
+	LDR R0, R5, #4
+	ADD R0, R0, R0
+	STR R0, R5, #4
+
+	; i <<= 1
+	LDR R0, R5, #0
+	ADD R0, R0, R0
+	STR R0, R5, #0
+
+	BRnzp MULTIPLY_LOOP_TOP
+
+MULTIPLY_LOOP_EXIT
+	; return accum
+	LDR R0, R5, #-1
+	STR R0, R5, #3
+
+	LDR R7, R5, #2
+	LDR R5, R5, #1
+	ADD R6, R6, #4 ; LV+2
+	RET
 
 STACK_BOTTOM .FILL x4000
 
